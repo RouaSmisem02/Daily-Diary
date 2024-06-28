4f01c2447d8757d88c5079f8d1ed1dcec813a547
Roua Smisem <rouasmisem02@gmail.com>
2024-06-28 18:29:40 +0300

    Add project files.

---
+++ DiaryManager/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DiaryManager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ DiaryManagerTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace DiaryManagerTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
